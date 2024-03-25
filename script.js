cc519b4c2171712708bc48d487317774e9b4df42
amarispurice <amaris.purice@emmanuel.ro>
2024-03-25 15:48:56 +0200

    am adaugat acoperisul

--- script.js
+++ script.js
@@ -9,4 +9,7 @@ function draw() {
     //dreptunghiul
         fill("pink");
         rect(100, 200 ,300, 200);
+     //acoperis
+        fill("purple");
+        triangle(80, 200, 250, 60, 420, 200);
 }
