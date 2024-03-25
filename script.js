38f3b94f5b3fce72727fd9dc2ee992ee5d495c17
amarispurice <amaris.purice@emmanuel.ro>
2024-03-25 15:47:55 +0200

    am adaugat un dreptunghi pentru casa

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function setup() {
     createCanvas(700, 500);
-    background("lightblue")
+    background("lightblue");
     }
+function draw() {
+    //casa
+        fill("white");
+        stroke("black");
+    //dreptunghiul
+        fill("pink");
+        rect(100, 200 ,300, 200);
+}
